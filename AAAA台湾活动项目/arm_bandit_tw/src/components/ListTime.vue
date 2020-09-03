7ca7748a0266879a717c55db70ab894eac695605
bbagoodprogrammer <chenchaojun19@163.com>
2020-09-03 18:52:18 +0800

    9.3

--- AAAA台湾活动项目/arm_bandit_tw/src/components/ListTime.vue
+++ AAAA台湾活动项目/arm_bandit_tw/src/components/ListTime.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="time">
-        {{time}}
-    </div>
+  <div class="time">
+    {{time}}
+  </div>
 </template>
 
 <script>
- import getDate from "../utils/getDate.js"
+import getDate from "../utils/getDate.js"
 export default {
-    props:['item'],
-    computed:{
-        time(){
-            return getDate(new Date(Number(this.item*1000)),'pai')
-        }
+  props: ['item'],
+  computed: {
+    time() {
+      console.log(this.item)
+      return getDate(new Date(Number(this.item * 1000)), 'rule')
     }
+  }
 }
 </script>
 
 <style>
-
 </style>
